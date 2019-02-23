2da62678981a68725a5f107cb102d9dc6331fc10
Mistergix <nicolas.ruche.entreprise@gmail.com>
2019-02-23 16:04:25 +0100

    Updates

--- sw.js
+++ sw.js
@@ -2,24 +2,24 @@ importScripts('/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/_nuxt/0522458a4fb6a037848f.js",
-    "revision": "dcb897636248b01505e03ad2f9f6f8c8"
+    "url": "/_nuxt/342d6f971279c1136150.js",
+    "revision": "fa7c684797fc516a250b6d944b9bad9c"
   },
   {
-    "url": "/_nuxt/1faeda4e543b326a5a8f.js",
-    "revision": "7415aaceb96d6480e9fc91ee2c71d201"
+    "url": "/_nuxt/3e581d9af59adcc53b9f.js",
+    "revision": "42b67ab8dd4a69666c70178c66205c6e"
   },
   {
-    "url": "/_nuxt/78bbcd0a8995b70dc277.js",
-    "revision": "f5a27b6cf84fd3fa130be624d728800c"
+    "url": "/_nuxt/5da913df51eb281634e2.js",
+    "revision": "866a6900342ac9eeaa7c34020d65f4d3"
   },
   {
-    "url": "/_nuxt/78e756e0d8957021e66b.js",
-    "revision": "696e50933299547d3de733e63a51dcc3"
+    "url": "/_nuxt/a2bd8469a007e36f4883.js",
+    "revision": "1d37b83ad050f8ee805c1bee707d1a4e"
   },
   {
-    "url": "/_nuxt/91fc5a390d7698923f9c.js",
-    "revision": "5323186f3f8114e2f941e4f68dfd0c74"
+    "url": "/_nuxt/a6d93834d9583b734de1.js",
+    "revision": "4f044a47a0626b833f2b569cc8b271f9"
   }
 ], {
   "cacheId": "client",
